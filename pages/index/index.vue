785c5dbbdc6a41e93b3aac7840300122f8182e06
2841692433 <2841692433@qq.com>
2026-08-13 13:49:19 +0800

    feat(index): 修改语言切换弹窗样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -702,7 +702,8 @@ export default {
 					break;
 				case 'language':
 					uni.showActionSheet({
-						itemList: ['简体中文', 'English(US)'],
+						itemList: ['简体中文', 'English(The United States)'],
+						itemColor: '#3880FC',
 						success: (res) => {
 							const lang = res.tapIndex === 0 ? 'zh-Hans' : 'en';
 							uni.setLocale(lang);
